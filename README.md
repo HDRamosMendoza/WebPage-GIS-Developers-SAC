ec7b2c84cc806f6cecac842d1b5050d0417f692e
Daniel Ramos <hramosm@contraloria.gob.pe>
2025-03-28 15:57:32 -0500

    Se agrega algunas librias de fuentes y estilos

--- README.md
+++ README.md
@@ -52,3 +52,16 @@ export default tseslint.config({
   },
 })
 ```
+
+
+
+npm install @mui/material @emotion/react @emotion/styled
+
+npm install @fontsource/roboto
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+npm install @mui/icons-material
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { Button } FROM "@mui/material"; //Importacion nombrada
+//import Button from "@mui/material/Button"; //Importación por defecto
 
-function App() {
-  const [count, setCount] = useState(0)
+import {Button, Container} from "@mui/material";
+
+export default function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <h1>Hola Heber</h1>
+      <Button variant="contained">Mi primer button</Button>
+    </Container>
+  );
 }
-
-export default App
